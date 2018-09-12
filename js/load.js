d76e764726abc2b64077dfad5f2644cdc50cde85
sawshaw <843506585@qq.com>
2018-09-12 18:43:24 +0800

    嗯嗯嗯
    呃呃呃从v

--- js/load.js
+++ js/load.js
@@ -1,6 +1,20 @@
 $(function(){
 		$("header").load("header.html",function() {
-	});
+		});
 		$("footer").load("footer.html",function(){
 		});
 });
+function loadHtml(){
+	try{
+		xmlObj.load(xmldoc); //chrome浏览器在这一行会报错，document对象没有load()方法。
+	}catch(e){ //捕捉异常
+		//webkit BUG,chrome etc.
+		xmlObj = new XMLHttpRequest(); //用AJAX中常见的套路来就可以解决了，不影响IE、FIREFOX的原生加载XML
+		xmlObj.overrideMimeType("text/xml");
+		xmlObj.open("GET", xmldoc, false);
+		xmlObj.send(null);
+		xmlResult = xmlObj.responseXML;
+		return xmlResult;
+	}
+	return xmlObj.documentElement;
+}
